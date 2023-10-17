eb650a57ddd844cac066f0e4507d9ae42c1ecf66
Statistic1an <vintzoglou@cmtprooptiki.gr>
2023-10-17 16:06:58 +0300

    message

--- app.py
+++ app.py
@@ -88,13 +88,14 @@ def main():
         st.metric(label="Αριθμός Ολοκληρωμένων Ερωτηματολογίων",value=dfdata["submitdate"].count())
 
     with col2:
+        st.write("ghgg")
        # idiotita_counts=dfdata["idiotita"].value_counts()
         #fig = px.pie(dfdata,values=idiotita_counts.values, names=idiotita_counts.index, title="Ιδιότητα Ερωτηθέντων;", hole=0.0)
         # Customize the layout if needed
         #fig.update_traces(textposition='inside', textinfo='percent+label')
         # Display the chart in your Streamlit app
         #st.plotly_chart(fig)
-        
+
     with col3:
         st.write("Under construction")
         fig=px.bar(dfdata,x=dfdata["idiotita"].value_counts().index,y=dfdata["idiotita"].value_counts().values,title="Ιδιότητα Ερωτηθέντων")
