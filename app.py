edc41e15cbe41034aeb8edf89e831f1abde09cf8
Statistic1an <vintzoglou@cmtprooptiki.gr>
2023-10-17 17:42:11 +0300

    message

--- app.py
+++ app.py
@@ -105,7 +105,7 @@ def main():
     # Print the reshaped DataFrame
     reshaped_data=reshaped_data.rename(columns={"": "NAN"})
     st.write(reshaped_data)
-    st.write(reshaped_data["l3"].value_counts())
+    st.write(dfdata["l3"].value_counts())
     
     ###################################################################################################################
     ###############################################START VAGGELIS######################################################
