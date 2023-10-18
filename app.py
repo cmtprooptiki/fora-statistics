02bf27bc50d73a555040c54fe87f5ac3c3af208e
Statistic1an <vintzoglou@cmtprooptiki.gr>
2023-10-18 15:06:39 +0300

    message

--- app.py
+++ app.py
@@ -116,119 +116,18 @@ def main():
     # ydata=np.asanyarray(reshaped_data.index)
     # st.write(ydata)
 
-
-########################################################################################################################################################
-############################################################################arxi 1o test diagramma#####################################################
-########################################################################################################################################################
-    # import plotly.graph_objects as go
-
-    # top_labels = ['Strongly<br>agree', 'Agree', 'Neutral', 'Disagree',
-    #             'Strongly<br>disagree']
-
-    # colors = ['rgba(38, 24, 74, 0.8)', 'rgba(71, 58, 131, 0.8)',
-    #         'rgba(122, 120, 168, 0.8)', 'rgba(164, 163, 204, 0.85)',
-    #         'rgba(190, 192, 213, 1)']
-
-    # x_data = xdata
-
-    # y_data = ydata
-
-    # fig = go.Figure()
-
-    # for i in range(0, len(x_data[0])):
-    #     for xd, yd in zip(x_data, y_data):
-    #         fig.add_trace(go.Bar(
-    #             x=[xd[i]], y=[yd],
-    #             orientation='h',
-    #             marker=dict(
-    #                 color=colors[i],
-    #                 line=dict(color='rgb(248, 248, 249)', width=1)
-    #             )
-    #         ))
-
-    # fig.update_layout(
-    #     xaxis=dict(
-    #         showgrid=False,
-    #         showline=False,
-    #         showticklabels=False,
-    #         zeroline=False,
-    #         domain=[0.15, 1]
-    #     ),
-    #     yaxis=dict(
-    #         showgrid=False,
-    #         showline=False,
-    #         showticklabels=False,
-    #         zeroline=False,
-    #     ),
-    #     barmode='stack',
-    #     paper_bgcolor='rgb(248, 248, 255)',
-    #     plot_bgcolor='rgb(248, 248, 255)',
-    #     margin=dict(l=120, r=10, t=140, b=80),
-    #     showlegend=False,
-    # )
-
-    # annotations = []
-
-    # for yd, xd in zip(y_data, x_data):
-    #     # labeling the y-axis
-    #     annotations.append(dict(xref='paper', yref='y',
-    #                             x=0.14, y=yd,
-    #                             xanchor='right',
-    #                             text=str(yd),
-    #                             font=dict(family='Arial', size=14,
-    #                                     color='rgb(67, 67, 67)'),
-    #                             showarrow=False, align='right'))
-    #     # labeling the first percentage of each bar (x_axis)
-    #     annotations.append(dict(xref='x', yref='y',
-    #                             x=xd[0] / 2, y=yd,
-    #                             text=str(xd[0]) + '%',
-    #                             font=dict(family='Arial', size=14,
-    #                                     color='rgb(248, 248, 255)'),
-    #                             showarrow=False))
-    #     # labeling the first Likert scale (on the top)
-    #     if yd == y_data[-1]:
-    #         annotations.append(dict(xref='x', yref='paper',
-    #                                 x=xd[0] / 2, y=1.1,
-    #                                 text=top_labels[0],
-    #                                 font=dict(family='Arial', size=14,
-    #                                         color='rgb(67, 67, 67)'),
-    #                                 showarrow=False))
-    #     space = xd[0]
-    #     for i in range(1, len(xd)):
-    #             # labeling the rest of percentages for each bar (x_axis)
-    #             annotations.append(dict(xref='x', yref='y',
-    #                                     x=space + (xd[i]/2), y=yd,
-    #                                     text=str(xd[i]) + '%',
-    #                                     font=dict(family='Arial', size=14,
-    #                                             color='rgb(248, 248, 255)'),
-    #                                     showarrow=False))
-    #             # labeling the Likert scale
-    #             if yd == y_data[-1]:
-    #                 annotations.append(dict(xref='x', yref='paper',
-    #                                         x=space + (xd[i]/2), y=1.1,
-    #                                         text=top_labels[i],
-    #                                         font=dict(family='Arial', size=14,
-    #                                                 color='rgb(67, 67, 67)'),
-    #                                         showarrow=False))
-    #             space += xd[i]
-
-    # fig.update_layout(annotations=annotations)
-
-    # st.plotly_chart(fig)
-########################################################################################################################################################
-############################################################################telos 1o test diagramma#####################################################
-########################################################################################################################################################
-
 ########################################################################################################################################################
 ############################################################################arxi 2o test diagramma######################################################
 ########################################################################################################################################################
 
     # #Creation of percentage df(every cell is the %of total of the row)
-    # row_sums= reshaped_data.sum(axis=1)
-    # percentage_data= round(reshaped_data.divide(row_sums,axis=0) *100,1)
-    # percentage_data = percentage_data.rename({'l1':"Η ΕΚΑΠΥ θα βελτιώσει τη διαδικασία προμηθειών φαρμάκου στα νοσοκομεία.","l2":"Η προμήθεια φαρμάκων μέσω της ΕΚΑΠΥ θα συμβάλει στη μείωση των δαπανών.","l3":"Η διεξαγωγή κλινικών μελετών στα νοσοκομεία βελτιώνει την ποιότητα των παρεχόμενων υπηρεσιών.","l4":"Η εφαρμογή του πλαισίου διασφάλισης Ποιότητας του ΟΔΙΠΥ στα νοσοκομεία θα βελτιώσει την ποιότητα των παρεχόμενων υπηρεσιών.","l5":"Η εφαρμογή του συστήματος DRGs θα βελτιώσει τη διαδικασία αποζημίωσης περιστατικών στα νοσοκομεία.","l6":"Η εφαρμογή του συστήματος DRGs θα βελτιώσει τη διαδικασία κατάρτισης και ελέγχου νοσοκομειακού προϋπολογισμού."})
-    # percentage_data.reset_index(drop=False, inplace=True)
-    # st.write("This is the percentage data where every cell is the percentage(%) of total for every row",percentage_data)
+    # Set the 'question' column as the index
+    prep=reshaped_data
+    prep.set_index('question', inplace=True)
+    row_sums= prep.sum(axis=1)
+    percentage_data= round(prep.divide(row_sums,axis=0) *100,1)
+    percentage_data.reset_index(drop=False, inplace=True)
+    st.write("This is the percentage data where every cell is the percentage(%) of total for every row",percentage_data)
 
     # # Populate the variables from the CSV
     # questions = d2.question
