363a5d0ff1257bbec878dfb148faef4212a5bcb8
Statistic1an <vintzoglou@cmtprooptiki.gr>
2023-10-19 16:11:16 +0300

    message

--- app.py
+++ app.py
@@ -185,7 +185,7 @@ def main():
         # # )
         st.write(chart_datat)
         #st.bar_chart(chart_datat)
-        chart_data11 = pd.melt(chart_datat.reset_index(), id_vars=["4"])
+        chart_data11 = pd.melt(chart_datat,var_name="variable", value_name="value")
         st.write(chart_data11)
 
         # Horizontal stacked bar chart
