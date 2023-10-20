8aaa2010d1ed01139d917d7f6869f1b0de4e972d
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-10-20 18:04:17 +0300

    message

--- app.py
+++ app.py
@@ -41,7 +41,8 @@ def main():
   
     st.set_page_config(page_title="Fora Survey Visualization",layout="wide")
 
-
+    with open( "style.css" ) as css:
+        st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
 
     col1, col2, col3 = st.columns(3)
 
