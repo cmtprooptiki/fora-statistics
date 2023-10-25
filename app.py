c224bd2ad28f9225e3bce57e55d663a54c61c15c
Ilias Zampetakis <eliaszabetakis@hotmail.com>
2023-10-25 11:18:30 +0300

    message

--- app.py
+++ app.py
@@ -333,8 +333,8 @@ def main():
                     order=alt.Order("variable", sort="ascending"),
                 )
                 .properties(
-    width=200,
-    height=100
+    width='container',
+    height='container'
 )
 .configure_axis(
     labelFontSize=10,
